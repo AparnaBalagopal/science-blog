a8b17bc846b27e41642c881c8713659f709963b4
admin <aparnabalagopal328@gmail.com>
2016-07-25 13:07:20 +0530

    added html pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^products/$', views.products, name='products'),
+    url(r'^services/$', views.services, name='services'),
     url(r'^contact/$', views.contact, name='contact'),
     url(r'^about/$', views.about, name='about'),
     
--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,13 @@ from django.shortcuts import redirect
 def home(request):
     return render(request, 'blog/home.html', {})
 
+def products(request):
+    return render(request, 'blog/products.html', {})
+
+def services(request):
+    return render(request, 'blog/services.html', {})
+
+
 def about(request):
     return render(request, 'blog/about.html', {})
 
