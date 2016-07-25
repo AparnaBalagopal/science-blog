ca113074cb60fb68cb6618f8d0e6ee8c8dfef3f9
admin <aparnabalagopal328@gmail.com>
2016-07-25 12:26:35 +0530

    change first page

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.about, name='about'),
+    url(r'^$', views.home, name='home'),
     url(r'^contact/$', views.contact, name='contact'),
+    url(r'^about/$', views.about, name='about'),
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Post
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+
+
+def home(request):
+    return render(request, 'blog/home.html', {})
 
 def about(request):
     return render(request, 'blog/about.html', {})
 
 def contact(request):
 	return render(request, 'blog/contact.html', {})
+
